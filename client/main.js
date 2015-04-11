3b3976a566725d5bfb9167dfc01b6b1aaf88cd8e
Eugene Mirotin <emirotin@gmail.com>
2015-04-11 14:03:36 +0300

    editing mode

--- client/main.js
+++ client/main.js
@@ -6,6 +6,23 @@
 		data: {}
 	});
 
+	app.on('begin-edit', function(event, i, currentTask){
+		app.set('todos.' + i + '.isEditing', true);
+		app.set('todos.' + i + '.newTask', currentTask);
+		return false;
+	});
+
+	app.on('save-edit', function(event, i, newTask){
+		app.set('todos.' + i + '.isEditing', false);
+		app.set('todos.' + i + '.task', newTask);
+		return false;
+	});
+
+	app.on('cancel-edit', function(event, i){
+		app.set('todos.' + i + '.isEditing', false);
+		return false;
+	});
+
 	fetch('/api/todos')
 	.then(function(response) {
 		return response.json();
